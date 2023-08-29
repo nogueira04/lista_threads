0fae294377dd9ec8bbf5825a640c84b384edc0c5
Mariana <mariana.guimaraesc@ufpe.br>
2023-08-29 15:22:24 -0300

    questao 6 static almost done

--- questao_6/questao_6.cpp
+++ questao_6/questao_6.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <pthread.h>
 #include <vector>
+#include <cmath>
 
 #define STATIC 0
 #define DYNAMIC 1
@@ -18,7 +19,7 @@ typedef struct {
 
 int OMPNumThreads = 6; 
 void ompFor(int inicio, int passo, int final, int schedule, int chunk_size, void (*f)(int));
-void* rotinaStatic(void* data);
+void*rotinaStatic(void* data);
 
 int main(){
     
@@ -34,15 +35,14 @@ int main(){
 
 void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, void (*f)(int)) {
 
-    pthread_t threadArray[OMPNumThreads];
+    
 
-    for(int i = inicio; i < final;i+=passo){
+    
         switch(schedule) {
             case STATIC: {
                 int ratio = (final - inicio) / passo; 
                 int ratioRemainder = (final - inicio) % passo; 
                 int iterationDivide = ratio / (OMPNumThreads * chunk_size); 
-                int threadsMultiply = OMPNumThreads * chunk_size;
                 int iterationDivideRemainder = ratio % (OMPNumThreads * chunk_size); 
                 pthread_t threadArray[OMPNumThreads];
 
@@ -64,60 +64,88 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
                         for (int i = 0; i < OMPNumThreads; i++) pthread_join(threadArray[i], NULL);
                     }
                 } else {
+                    int indexTemp = 0;
+                    bool endLoop = false;
                     int intervalIndex = 0;
                     int chunkSizeSum = 0;
                     staticData** staticDataMatrix = new staticData*[OMPNumThreads]; 
                     for (int i = 0; i < OMPNumThreads; i++) staticDataMatrix[i] = new staticData[iterationDivide];                 
 
-                    while (ratio >= threadsMultiply) {
-                        for (int i = 0; i < OMPNumThreads; i++) {
-                            staticDataMatrix[i][intervalIndex].beginIndex = (passo * chunk_size) * (i + (OMPNumThreads * intervalIndex));
-                            staticDataMatrix[i][intervalIndex].endIndex = staticDataMatrix[i][intervalIndex].beginIndex + ((chunk_size - 1) * passo);
-                        
+                    while(!endLoop){
+                        staticDataMatrix[indexTemp][intervalIndex].beginIndex = (passo * chunk_size) * (indexTemp + (OMPNumThreads * intervalIndex));
+                        staticDataMatrix[indexTemp][intervalIndex].endIndex = staticDataMatrix[indexTemp][intervalIndex].beginIndex + ((chunk_size - 1) * passo);
+                        indexTemp++;
+                        ratio = ratio - chunk_size;
+                        if(indexTemp == OMPNumThreads - 1) {
+                            indexTemp = 0;
+                            intervalIndex++;
                         }
-                        intervalIndex++;
-                        ratio -= threadsMultiply;
                         chunkSizeSum += chunk_size;
+                        if(ratio/((OMPNumThreads - 1) - indexTemp) > chunk_size) endLoop = true;
                     }
+
                     
-                    if(ratio % OMPNumThreads == 0){
-                        int ratioPerThread = ratio/OMPNumThreads;
-                        int lastIndex = -1;
+                    if(ratio % ((OMPNumThreads - 1) - indexTemp) == 0){
+                        int ratioPerThread = ratio/((OMPNumThreads - 1) - indexTemp);
+                        int lastIndex = staticDataMatrix[indexTemp][intervalIndex].endIndex;
+        
                         
-                        for (int i = 0; i < OMPNumThreads; i++) {
-                            staticDataMatrix[i][intervalIndex].beginIndex = passo * lastIndex + 1;
-                            staticDataMatrix[i][intervalIndex].endIndex = staticDataMatrix[i][intervalIndex].beginIndex + ratioPerThread;
+                        while(indexTemp < (OMPNumThreads - 1)){
+                            indexTemp++;
+                            staticDataMatrix[indexTemp][intervalIndex].beginIndex = passo * lastIndex + 1;
+                            staticDataMatrix[indexTemp][intervalIndex].endIndex = staticDataMatrix[indexTemp][intervalIndex].beginIndex + (ratioPerThread - 1)*passo;
                             
-                            lastIndex = staticDataMatrix[i][intervalIndex].endIndex;
+                            lastIndex = staticDataMatrix[indexTemp][intervalIndex].endIndex;
+                            if(indexTemp == OMPNumThreads - 1) {
+                            indexTemp = 0;
+                            intervalIndex++;
+                            }
                         } 
+                        for (int i = 0; i < OMPNumThreads; i++) pthread_create(&threadArray[i], NULL, rotinaStatic, (void*) &staticDataMatrix[i][intervalIndex]);
+                        for (int i = 0; i < OMPNumThreads; i++) pthread_join(threadArray[i], NULL);
 
                     } else {
-                        int index = 0;
+                        
                         int ratioPerThreadRemainder = ratio % OMPNumThreads; //2
                         int ratioInitial = (final - inicio) / passo; //12 
-                        int ratioTemp = 0;
-                        int ratioRemainder = ratioInitial - chunkSizeSum; //12 - 4 = 8
-                        int ceilRatioRemainder = ceil(ratioRemainder / OMPNumThreads) + 1; //8/5 = 2 + 1 = 3
-                        while(ratioTemp >= ratioRemainder){
-                           
-                                staticDataMatrix[index][intervalIndex].beginIndex = ceilRatioRemainder;
-                                staticDataMatrix[index][intervalIndex].endIndex = staticDataMatrix[i][intervalIndex].beginIndex + ceilRatioRemainder;
-                                index++;
-                                ratioTemp = ratioRemainder - ceilRatioRemainder;
+                        int ratioRemainder = ratioInitial - chunkSizeSum; //13 - 6 = 7
+                        int ceilRatioRemainder = ceil(ratioRemainder / ratioPerThreadRemainder) + 1; //7/2 = 3 + 1 = 4
+                        int lastIndex = staticDataMatrix[indexTemp][intervalIndex].endIndex;
+                        while(ratioRemainder >= ceilRatioRemainder){
+                            indexTemp++;
+                            staticDataMatrix[indexTemp][intervalIndex].beginIndex = (lastIndex + 1) * passo;
+                            staticDataMatrix[indexTemp][intervalIndex].endIndex = staticDataMatrix[indexTemp][intervalIndex].beginIndex + (ceilRatioRemainder - 1) * passo;
+        
+                            lastIndex = staticDataMatrix[indexTemp][intervalIndex].endIndex;
+                            if(indexTemp == OMPNumThreads - 1) {
+                            indexTemp = 0;
+                            intervalIndex++;
+                            }
+                            ratioRemainder -= ceilRatioRemainder;
                                 
                         } 
+                        while(indexTemp < (OMPNumThreads - 1)){
+                            indexTemp++;
+                            staticDataMatrix[indexTemp][intervalIndex].beginIndex = lastIndex + 1;
+                            staticDataMatrix[indexTemp][intervalIndex].endIndex = staticDataMatrix[indexTemp][intervalIndex].beginIndex + ratioRemainder;
+
+                            lastIndex = staticDataMatrix[indexTemp][intervalIndex].endIndex;
+                            
 
                         }
-//12,5,2 : 2,2,3,3,2
+                         for (int i = 0; i < OMPNumThreads; i++) {
+                            pthread_create(&threadArray[i], NULL, rotinaStatic, (void*) &staticDataMatrix[i][intervalIndex]);
+                        }
 
-
-
-                        
+                        for (int i = 0; i < OMPNumThreads; i++) pthread_join(threadArray[i], NULL);
                     }
-                }
 
+                    }
+                       
             }
             break;
+            
+
 
             case DYNAMIC:
                 //
@@ -129,5 +157,6 @@ void omp_for(int inicio, int passo, int final, int schedule, int chunk_size, voi
                 //
             break;
         }
-    }
 }
+
+void* rotinaStatic(void* data);
